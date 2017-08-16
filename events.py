1b8da7cceb31aeafeb6dbce81c3f71738d1e1274
Brian Chan <i@b123400.net>
2017-08-17 00:48:33 +0300

    Show regions for error after rebuild

--- events.py
+++ events.py
@@ -5,7 +5,15 @@ import tempfile
 import os
 from functools import wraps
 
-from .command import start_server, stop_server, CodeCompleteThread, add_import, get_module_imports, get_type
+from .command import ( start_server
+                     , stop_server
+                     , CodeCompleteThread
+                     , add_import
+                     , get_module_imports
+                     , get_type
+                     , rebuild )
+
+from .error import error_manager
 
 def is_project_dir(path):
     return os.path.isfile(os.path.join(path, 'package.json'))
@@ -187,6 +195,53 @@ class TypeHintEventListener(PurescriptViewEventListener):
         500,    # max width
         500,    # max height
         on_navigate)
+
+class RebuildEventListener(sublime_plugin.EventListener):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @ignore_non_purescript
+    def on_post_save_async(self, view):
+        file_name = view.file_name()
+        if file_name is None:
+            return
+        project_path = find_project_dir(file_name)
+        errors = rebuild(project_path, file_name)
+
+        regions_and_errors = []
+        regions = []
+        for error in errors:
+            start = view.text_point(
+                error['position']['startLine']-1,
+                error['position']['startColumn']-1
+            )
+            end = view.text_point(
+                error['position']['endLine']-1,
+                error['position']['endColumn']-1
+            )
+            region = sublime.Region(start, end+1)
+
+            if region.size() <= 1:
+                # try to make the region bigger because
+                # zero width region is invisible
+                region = view.word(start)
+
+            regions_and_errors.append((region, error))
+            regions.append(region)
+
+        error_manager.set_errors(file_name, regions_and_errors)
+        view.add_regions("errors", regions,
+            "invalid.illegal",
+
+            # This thing does not exist in doc, but it exists in the default theme.
+            # It might break some days
+            "light_x_bright",
+
+            sublime.DRAW_NO_FILL |
+            sublime.DRAW_NO_OUTLINE |
+            sublime.DRAW_SQUIGGLY_UNDERLINE
+            )
 
 
 class ReplaceCommand(sublime_plugin.TextCommand):
