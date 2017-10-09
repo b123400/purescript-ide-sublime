10f779e798bfa4a1073162067474efbb8b37a10b
Brian Chan <i@b123400.net>
2017-10-09 18:13:52 +0800

    Show errors without position at current selection, so it at least get shown

--- ide/rebuild.py
+++ ide/rebuild.py
@@ -11,6 +11,7 @@ class RebuildEventListener(sublime_plugin.EventListener):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.phantom_sets = [] # [(view, phantomset)]
 
     @ignore_non_purescript
     def on_post_save_async(self, view):
@@ -22,7 +23,22 @@ class RebuildEventListener(sublime_plugin.EventListener):
 
         regions_and_errors = []
         regions = []
+        error_without_position = []
         for error in errors:
+            # it's possible to have error with no position like this
+            #
+            # {'suggestion': None,
+            # 'position': None,
+            # 'errorCode': 'UnusableDeclaration',
+            # 'moduleName': 'Localization.Smolder',
+            # 'errorLink': 'https://github.com/purescript/documentation/blob/master/errors/UnusableDeclaration.md',
+            # 'message': "  The declaration withEvent is unusable.\n  This happens when a constraint couldn't possibly have enough information to work out which instance is required.\n",
+            # 'filename': None
+            # }
+            if error['position'] is None:
+                print(error)
+                error_without_position.append(error)
+                continue
             start = view.text_point(
                 error['position']['startLine']-1,
                 error['position']['startColumn']-1
@@ -54,3 +70,29 @@ class RebuildEventListener(sublime_plugin.EventListener):
             sublime.DRAW_NO_OUTLINE |
             sublime.DRAW_SQUIGGLY_UNDERLINE
             )
+
+        ps = sublime.PhantomSet(view)
+
+        ps.update([sublime.Phantom(
+                    view.sel()[0],
+                    "".join(['<p>%s</p>' % s.replace(' ', '&nbsp;')
+                        for s in error['message']
+                            .split('\n')]),
+                    sublime.LAYOUT_BLOCK) for error in error_without_position])
+
+        self.delete_phantom_in_view(view)
+        self.phantom_sets.append((view, ps))
+
+    @ignore_non_purescript
+    def on_close(self, view):
+        self.delete_phantom_in_view(view)
+
+    def delete_phantom_in_view(self, view):
+        i = 0
+        index = None
+        for (v, _) in self.phantom_sets:
+            if v == view:
+                index = i
+            i += 1
+        if index is not None:
+            del self.phantom_sets[index]
